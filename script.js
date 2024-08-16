455a3b4cfa7407b9666d1840dd6d688456a9881c
arih-kz <hirashahnaz3@gmail.com>
2024-08-16 16:07:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+function openlah_fort() {
+    window.open("lah_fort.html", "_blank");
+}
+
+function openmizar_quaid() {
+    window.open("mizar_quaid.html", "_blank");
+}
+
+function openHiran_Minar() {
+    window.open("Hiran_Minar.html", "_blank");
+}
+
+function openMohatta_Palace_Museum() {
+    window.open("Mohatta_Palace_Museum.html", "_blank");
+}
+
+function openDerawar_Fort() {
+    window.open("Derawar_Fort.html", "_blank");
+}
+
+function openRanikot_Fort() {
+    window.open("Ranikot_Fort.html", "_blank");
+}
+
+function openShalamar_Garden() {
+    window.open("Shalamar_Garden.html", "_blank");
+}
+
+function openTomb_of_Jahangir() {
+    window.open("Tomb_of_Jahangir.html", "_blank");
+}
+
+function openHunza_Valley() {
+    window.open("Hunza_Valley.html", "_blank");
+}
+
+function openLake_Saif_Ul_Malook() {
+    window.open("Lake_Saif_Ul_Malook.html", "_blank");
+}
+
+function openFaisal_mosque() {
+    window.open("Faisal_mosque.html", "_blank");
+}
+
+function openMinar_pakistan() {
+    window.open("Minar_pakistan.html", "_blank");
+}
+
+function openArts_Crafts() {
+    window.open("Arts_Crafts.html", "_blank");
+}
+
+function openFestivals() {
+    window.open("Festivals.html", "_blank");
+}
+
